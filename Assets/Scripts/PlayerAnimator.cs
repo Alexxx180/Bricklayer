27c3e92bdfcbe750ea4f858c35f82959b608d189
Студент ПТК <student@ptk.ru>
2021-10-04 14:25:12 +0300

    Alpha 1.35 | Animations

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    // Update is called once per frame
+    void Update()
+    {
+        _animator.SetBool("isRun", Input.GetKey(KeyCode.W));
+        _animator.SetBool("isAttack", Input.GetButtonDown("Fire1"));
+    }
+}
